725d65eba1b87d577371324ba0c33ec13d0b80af
Manoj Nayak <emails.manoj@gmail.com>
2025-10-20 06:27:04 +0000

    The logo should be an owl, and the theme of the website should give an o

---
+++ src/components/icons/OwlIcon.tsx
@@ -0,0 +1,23 @@
+export function OwlIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12 22c-5.523 0-10-4.477-10-10S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z" />
+      <path d="M7 9a1 1 0 0 0 2 0V8a1 1 0 0 0-2 0v1zm10 0a1 1 0 0 0 2 0V8a1 1 0 0 0-2 0v1z" />
+      <path d="M12 18c4 0 6-3 6-3H6s2 3 6 3z" />
+      <path d="M12 14c-1.417 0-2.756-.63-3.625-1.667C7.508 11.3 7 10.15 7 9" />
+      <path d="M12 14c1.417 0 2.756-.63 3.625-1.667C16.492 11.3 17 10.15 17 9" />
+      <path d="M12 8a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 1 0v-2A.5.5 0 0 0 12 8z" />
+    </svg>
+  );
+}
