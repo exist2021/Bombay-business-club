1bc57b4f0e4ac3ec289887476622b67d39369164
Manoj Nayak <emails.manoj@gmail.com>
2025-11-12 00:45:03 +0000

    This is the script tag for google analytics <script type="module">   //

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,31 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&display=swap" rel="stylesheet" />
         <Script src="https://elfsightcdn.com/platform.js" strategy="lazyOnload" />
+        <Script id="firebase-analytics" strategy="afterInteractive">
+          {`
+            // Import the functions you need from the SDKs you need
+            import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+            import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+            // TODO: Add SDKs for Firebase products that you want to use
+            // https://firebase.google.com/docs/web/setup#available-libraries
+
+            // Your web app's Firebase configuration
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+            const firebaseConfig = {
+              apiKey: "AIzaSyBEumt4pBAUM52NB8eff5lObx2ABJF_IOA",
+              authDomain: "studio-6153032827-bcb85.firebaseapp.com",
+              projectId: "studio-6153032827-bcb85",
+              storageBucket: "studio-6153032827-bcb85.firebasestorage.app",
+              messagingSenderId: "690261451295",
+              appId: "1:690261451295:web:f11bc06145ff5e65146b2a",
+              measurementId: "G-KLXGV1RN65"
+            };
+
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col bg-background">
         <Header />
